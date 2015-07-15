538af243596da59483a19ff02987d3b105fa7517
Nicolas Boulay <cherry@etherale.org>
2015-07-15 22:26:55 +0200

    tstring main

--- src/tstring.ml
+++ src/tstring.ml
@@ -5,8 +5,18 @@ type tstring = (* to avoid recopy better than string list *)
 let  tstring_to_string t =
   let rec to_ a t =
     match t with 
-      | Node list -> List.fold_left to_ "" list
+      | Node list -> List.fold_left to_ a list
       | Str s -> a ^ s
   in
   to_ "" t
 
+let print t =
+  let rec to_ a t =
+    match t with 
+      | Node list -> List.fold_left to_ a list
+      | Str s -> print_string s
+  in
+  to_ (Str "") t
+
+let _ =
+  print ( Node [ Str "plop" ] )
